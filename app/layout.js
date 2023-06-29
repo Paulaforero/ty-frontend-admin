bcdc0b00c745e0aafb9c0c64579ec57a451a20bb
NØRA <joseandresrs0407@gmail.com>
2023-06-28 20:08:56 -0400

    feat: the configuration was completed

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'T&Y Admin Panel',
+  description: 'Panel de administración de la empresa de concesionarios T&Y',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
