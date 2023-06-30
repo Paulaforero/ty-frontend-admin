ef678fc30850c8f3380a19542f93ddf0bfc250e9
NØRA <joseandresrs0407@gmail.com>
2023-06-30 00:02:35 -0400

    feat: created dealerships mock data

---
+++ mock/dealerships.js
@@ -0,0 +1,122 @@
+const dealerships = [
+  {
+    rif: 'J-123456789',
+    name: 'Concesionaria A',
+    city_id: 1,
+    manager_id: 'M-987654321'
+  },
+  {
+    rif: 'J-234567890',
+    name: 'Concesionaria B',
+    city_id: 2,
+    manager_id: 'M-876543210'
+  },
+  {
+    rif: 'J-345678901',
+    name: 'Concesionaria C',
+    city_id: 3,
+    manager_id: 'M-765432109'
+  },
+  {
+    rif: 'J-456789012',
+    name: 'Concesionaria D',
+    city_id: 4,
+    manager_id: 'M-654321098'
+  },
+  {
+    rif: 'J-567890123',
+    name: 'Concesionaria E',
+    city_id: 5,
+    manager_id: 'M-543210987'
+  },
+  {
+    rif: 'J-678901234',
+    name: 'Concesionaria F',
+    city_id: 6,
+    manager_id: 'M-432109876'
+  },
+  {
+    rif: 'J-789012345',
+    name: 'Concesionaria G',
+    city_id: 7,
+    manager_id: 'M-321098765'
+  },
+  {
+    rif: 'J-890123456',
+    name: 'Concesionaria H',
+    city_id: 8,
+    manager_id: 'M-210987654'
+  },
+  {
+    rif: 'J-901234567',
+    name: 'Concesionaria I',
+    city_id: 9,
+    manager_id: 'M-109876543'
+  },
+  {
+    rif: 'J-012345678',
+    name: 'Concesionaria J',
+    city_id: 10,
+    manager_id: 'M-098765432'
+  },
+  {
+    rif: 'J-1234567890',
+    name: 'Concesionaria K',
+    city_id: 11,
+    manager_id: 'M-9876543210'
+  },
+  {
+    rif: 'J-2345678901',
+    name: 'Concesionaria L',
+    city_id: 12,
+    manager_id: 'M-8765432109'
+  },
+  {
+    rif: 'J-3456789012',
+    name: 'Concesionaria M',
+    city_id: 13,
+    manager_id: 'M-7654321098'
+  },
+  {
+    rif: 'J-4567890123',
+    name: 'Concesionaria N',
+    city_id: 14,
+    manager_id: 'M-6543210987'
+  },
+  {
+    rif: 'J-5678901234',
+    name: 'Concesionaria O',
+    city_id: 15,
+    manager_id: 'M-5432109876'
+  },
+  {
+    rif: 'J-6789012345',
+    name: 'Concesionaria P',
+    city_id: 16,
+    manager_id: 'M-4321098765'
+  },
+  {
+    rif: 'J-7890123456',
+    name: 'Concesionaria Q',
+    city_id: 17,
+    manager_id: 'M-3210987654'
+  },
+  {
+    rif: 'J-8901234567',
+    name: 'Concesionaria R',
+    city_id: 18,
+    manager_id: 'M-2109876543'
+  },
+  {
+    rif: 'J-9012345678',
+    name: 'Concesionaria S',
+    city_id: 19,
+    manager_id: 'M-1098765432'
+  },
+  {
+    rif: 'J-0123456789',
+    name: 'Concesionaria T',
+    city_id: 20,
+    manager_id: 'M-0987654321'
+  }
+];
