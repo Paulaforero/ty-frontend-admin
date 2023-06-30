93998021b8e1d3b6cdadba7bc12c58f57b62dfde
NØRA <joseandresrs0407@gmail.com>
2023-06-29 23:51:08 -0400

    feat: setted height and width screen size to body in root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <ThemeProvider theme={theme}>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className + 'w-screen h-screen'}>{children}</body>
       </ThemeProvider>
     </html>
   )
