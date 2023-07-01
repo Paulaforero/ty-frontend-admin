eb83b33d5f50f310e7a395fe55ea22c1c1ef7e18
Chocopaula <mariapforero2003@gmail.com>
2023-07-01 07:48:04 -0400

    feat: getting input data and color changes using theme

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -11,18 +11,38 @@ import {
   InputLabel,
   FormControl,
   Box,
+  CircularProgress,
 } from '@mui/material'
 import { useState } from 'react'
 import Image from 'next/image'
+import { useEffect } from 'react'
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false)
+
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+  }, [])
+  const [user, setUser] = useState({
+    user: '',
+    password: '',
+  })
 
   const handleClickShowPassword = () => setShowPassword(show => !show)
 
   const handleMouseDownPassword = event => {
     event.preventDefault()
   }
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+  const handleSubmit = e => {
+    e.preventDefault
+    setLoading(true)
+  }
+
   return (
     <>
       <Grid
@@ -32,7 +52,7 @@ export default function LoginPage() {
         alignItems="center"
       >
         <Typography
-          className="text-secondary mb-5"
+          className=" text-secondary mb-5 mt-3"
           variant="h4"
           textAlign="center"
         >
@@ -45,34 +65,42 @@ export default function LoginPage() {
         >
           "Comprometidos con el ambiente"
         </Typography>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Grid
             container
             direction="column"
-            justifyContent="center"
+            justifyContent="space-between"
             alignContent="center"
           >
             <TextField
-              id="standard-password-input"
+              id="standard-user-input"
               label="Usuario"
               variant="standard"
-              className="text-secondary mb-14 mt-8"
+              className="mb-14 mt-8"
+              color="secondary"
+              name="user"
+              onChange={handleChange}
             />
             <FormControl variant="standard">
-              <InputLabel htmlFor="standard-adornment-password">
+              <InputLabel
+                htmlFor="standard-adornment-password"
+                color="secondary"
+              >
                 Contraseña
               </InputLabel>
               <Input
                 id="standard-adornment-password"
                 type={showPassword ? 'text' : 'password'}
-                className="text-secondary"
+                color="secondary"
+                onChange={handleChange}
+                name="password"
                 endAdornment={
                   <InputAdornment position="end">
                     <IconButton
                       aria-label="toggle password visibility"
                       onClick={handleClickShowPassword}
                       onMouseDown={handleMouseDownPassword}
-                      className="text-secondary"
+                      color="secondary"
                     >
                       {showPassword ? <VisibilityOff /> : <Visibility />}
                     </IconButton>
@@ -84,19 +112,26 @@ export default function LoginPage() {
               variant="contained"
               size="medium"
               type="submit"
-              className="text-white mx-12 bg-primary mt-[90px]"
+              className="mx-12 mt-[90px]  font-semibold normal-case text-[14px]"
+              color="primary"
+              disabled={loading === true}
             >
-              Iniciar sesión
+              {loading ? (
+                <CircularProgress color="primary" size={24} />
+              ) : (
+                'Iniciar sesión'
+              )}
             </Button>
           </Grid>
         </form>
       </Grid>
-      <Box className="rounded-e-full	w-[358px] h-48 absolute left-0 bg-primary">
+
+      <Box className="rounded-e-full	w-[358px] h-48 fixed left-0 bottom-5 bg-primary">
         <Image
           src="/images/image_car_1.png"
           width={380}
           height={290}
-          alt="Carro_numero_1"
+          alt="car_1"
           className="absolute left-5 border-solid"
         />
       </Box>
