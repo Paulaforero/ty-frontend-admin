e55d1c532401b18e74f9043479233f8038341434
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-07-05 12:34:31 -0400

    feat: updated dealerships page to use the data page component

--- app/(private)/dealerships/page.jsx
+++ app/(private)/dealerships/page.jsx
@@ -1,131 +1,46 @@
 'use client'
 
-import {
-  Button,
-  Select,
-  Table,
-  TextField,
-  Divider,
-  Stack,
-  Card,
-  Typography,
-  Box,
-  Container,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  TablePagination,
-  TableFooter,
-  TableContainer,
-} from '@mui/material'
-import { dealerships } from '../../../mock/dealerships'
-import { useState } from 'react'
-import { MoreVertOutlined } from '@mui/icons-material'
-import IconMenu from '@/components/menu'
+import DataPageView from '@/components/data-page/data-page-view'
+import { dealerships } from '@/mock/dealerships'
 
 export default function DealershipsPage() {
-  const [rows, setRows] = useState(dealerships)
-  const [rowsPerPage, setRowsPerPage] = useState(10)
-  const [page, setPage] = useState(0)
+  const columns = {
+    rif: 'Rif',
+    name: 'Nombre',
+    city_id: 'Ciudad (ID)',
+    manager_id: 'Manager (ID)',
+  }
 
-  const handlePageChange = (event, newPage) => setPage(newPage)
+  const filters = [
+    {
+      label: 'Filtar por ciudad',
+      options: [
+        { label: 'uno', value: 1 },
+        { label: 'dos', value: 2 },
+        { label: 'tres', value: 3 },
+        { label: 'cuatro', value: 4 },
+        { label: 'cinco', value: 5 },
+      ],
+    },
+    {
+      label: 'Filtrar por manager',
+      options: [
+        { label: 'uno', value: 1 },
+        { label: 'dos', value: 2 },
+        { label: 'tres', value: 3 },
+        { label: 'cuatro', value: 4 },
+        { label: 'cinco', value: 5 },
+      ],
+    },
+  ]
 
   return (
-    <Box component="main" className="w-full h-full pt-9">
-      <Container
-        component="container"
-        className="flex flex-col justify-center items-center w-full mb-5"
-      >
-        <Box component="header" className="pb-4 w-full">
-          <Typography
-            variant="h4"
-            component="h2"
-            className="text-left"
-            color="secondary"
-          >
-            Concesionarios
-          </Typography>
-        </Box>
-
-        <Card
-          variant="elevation"
-          className="flex flex-col gap-5 px-10 py-6 w-full h-full mx-2 mb-10"
-        >
-          <Box component="section">
-            <Typography
-              variant="h5"
-              component="h3"
-              className="pb-2"
-              color="secondary"
-            >
-              Filtros de busqueda
-            </Typography>
-            <Stack direction="row" spacing={1}>
-              <Select className="flex-grow" />
-              <Select className="flex-grow" />
-              <Select className="flex-grow" />
-            </Stack>
-          </Box>
-
-          <Divider variant="middle" />
-
-          <Box
-            component="section"
-            className="flex flex-nowrap gap-2 justify-end mt-2"
-          >
-            <TextField
-              variant="outlined"
-              placeholder="Buscar por nombre..."
-            ></TextField>
-            <Button variant="contained" color="primary">
-              Agregar concesionario
-            </Button>
-          </Box>
-
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left">RIF</TableCell>
-                  <TableCell align="left">Nombre</TableCell>
-                  <TableCell align="left">Ciudad (ID)</TableCell>
-                  <TableCell align="left">Manager (ID)</TableCell>
-                  <TableCell align="left">Acciones</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {(rowsPerPage > 0
-                  ? dealerships.slice(
-                      page * rowsPerPage,
-                      page * rowsPerPage + rowsPerPage
-                    )
-                  : dealerships
-                ).map(dealership => (
-                  <TableRow key={dealership.rif}>
-                    <TableCell align="left">{dealership.rif}</TableCell>
-                    <TableCell align="left">{dealership.name}</TableCell>
-                    <TableCell align="left">{dealership.city_id}</TableCell>
-                    <TableCell align="left">
-                      {dealership.manager_id}
-                    </TableCell>
-                    <TableCell align="left">
-                      <IconMenu />
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            component="div"
-            rowsPerPage={rowsPerPage}
-            count={rows.length}
-            page={page}
-            onPageChange={handlePageChange}
-          />
-        </Card>
-      </Container>
-    </Box>
+    <DataPageView
+      title="Concesionarios"
+      createButtonLabel="Agregar concesionario"
+      columns={columns}
+      rows={dealerships}
+      filters={filters}
+    />
   )
 }
