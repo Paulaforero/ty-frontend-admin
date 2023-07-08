fac8d9bc8dff6bc8913ef4c73acbcd0a803cebab
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-07-08 15:00:25 -0400

    refactor: moved list item componentes to components root to use in other page components

---
+++ components/form-list-items/select-input-list-item.jsx
@@ -0,0 +1,50 @@
+import {
+  FormControl,
+  InputLabel,
+  ListItem,
+  MenuItem,
+  Select,
+  Typography,
+} from '@mui/material'
+
+export default function SelectInputListItem({
+  label,
+  placeholder,
+  value,
+  options,
+  name,
+  handleChange,
+}) {
+  return (
+    <ListItem className="flex flex-row">
+      <Typography variant="p" align="left" className="font-bold w-[15%]">
+        {label}
+      </Typography>
+      <FormControl fullWidth>
+        <InputLabel
+          id={`${name}-select`}
+          size="small"
+          className="items-center ml-7 lg:ml-9 w-[95%]"
+        >
+          {placeholder}
+        </InputLabel>
+        <Select
+          id={`${name}-select`}
+          size="small"
+          value={value}
+          label={placeholder}
+          name={name}
+          className="ml-7 lg:ml-9 w-[95%]"
+          onChange={handleChange}
+        >
+          <MenuItem value="">{placeholder}</MenuItem>
+          {options.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </ListItem>
+  )
+}
