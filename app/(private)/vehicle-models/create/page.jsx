cc8f3c8796e4894c910e58cdb92be0a2d0c78ac4
Chocopaula <mariapforero2003@gmail.com>
2023-07-09 00:30:05 -0400

    feature: added new information in inputs array

--- app/(private)/vehicle-models/create/page.jsx
+++ app/(private)/vehicle-models/create/page.jsx
@@ -19,22 +19,23 @@ export default function CreatePage() {
       label: 'Nombre:',
       type: 'text',
       name: 'name',
-      placeholder: 'Inserte el nombre',
-      required: true,
+      placeholder: 'Ingrese el nombre',
     },
     {
       label: 'Cantidad de asientos:',
-      type: 'text',
+      type: 'number',
       name: 'seat_count',
-      placeholder: 'Inserte la cantidad de asientos',
-      required: true,
+      placeholder: 'Ingrese la cantidad de asientos',
+      min: 2,
+      max:8
     },
     {
       label: 'Peso en Kg:',
       type: 'text',
       name: 'weigh_in_kg',
-      placeholder: 'Inserte el peso en kg',
+      placeholder: 'Ingrese el peso',
       required: true,
+      adornment: 'Kg',
     },
     {
       label: 'Octanaje:',
@@ -51,28 +52,28 @@ export default function CreatePage() {
       ],
       name: 'octane_rating',
       placeholder: 'Seleccione el octanaje',
-      required: true,
+
     },
     {
       label: 'Tipo de aceite de la caja:',
       type: 'text',
       name: 'gearbox_oil_type',
       placeholder: 'Ingrese el tipo de aceite de la caja',
-      required: false,
+      required: true,
     },
     {
       label: 'Tipo de aceite del motor:',
       type: 'text',
       name: 'engine_oil_type',
       placeholder: 'Ingrese el tipo de aceite del motor',
-      required: false,
+      required: true,
     },
     {
       label: 'Tipo de refrigerante:',
       type: 'text',
       name: 'engine_coolant_type',
       placeholder: 'Ingrese el tipo de refrigerante',
-      required: false,
+      required: true,
     },
   ]
 
