17a9b8722f04ddf4a99f219c1951872aa98c0e88
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-07-09 15:26:01 -0400

    feat: created details page view component

--- components/details-page-view/index.jsx
+++ components/details-page-view/index.jsx
@@ -7,31 +7,21 @@ import {
   IconButton,
   Typography,
   Button,
-  InputLabel,
-  Input,
   Stack,
   ListItem,
-  Select,
-  MenuItem,
 } from '@mui/material'
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
-import { useParams } from 'next/navigation'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
-export default function DetailsPageView() {
-  const dealership = {
-    rif: 'J-123456789',
-    name: 'Concesionaria A',
-    city_id: 1,
-    manager_id: 'M-987654321',
-  }
-
-  const { id } = useParams()
+export default function DetailsPage({ title, toEditButtonLabel, rows }) {
+  const pathname = usePathname()
 
   return (
     <Box component="main" className="w-full h-full pt-9">
       <Container
         component="container"
-        className="flex flex-row justify-center items-center h-full w-full mb-5"
+        className="flex flex-col justify-center items-center h-full w-full mb-5"
       >
         <Box component="header" className="pb-4 w-full">
           <IconButton size="large" color="secondary">
@@ -39,71 +29,49 @@ export default function DetailsPageView() {
           </IconButton>
         </Box>
 
-        <Box component="section" className="flex flex-row flex-grow gap-4 w-full">
+        <Box
+          component="section"
+          className="flex flex-row flex-grow gap-4 w-full"
+        >
           <Card
             variant="elevation"
             className="flex flex-col gap-5 px-10 py-6 h-full mx-2 mb-10 flex-grow"
           >
-            <Typography variant="h5" align="right">
-              Concesionario #{id}
+            <Typography variant="h4" align="left">
+              {title}
             </Typography>
 
-            <Stack>
-              <ListItem>
-                <Typography variant="p" align="left" className="font-bold">
-                  RIF:
-                </Typography>
-                <Input
-                  placeholder="Inserte el RIF..."
-                  value={dealership.rif}
-                  className="ml-5"
-                />
-              </ListItem>
-              <ListItem>
-                <Typography variant="p" align="left" className="font-bold">
-                  Nombre:
-                </Typography>
-                <Input
-                  placeholder="Inserte el nombre..."
-                  value={dealership.name}
-                  className="ml-5"
-                />
-              </ListItem>
-              <ListItem>
-                <Typography variant="p" align="left" className="font-bold">
-                  Ciudad:
-                </Typography>
-                <Select
-                  placeholder="Selecciona la ciudad..."
-                  value={dealership.city_id}
-                  className="ml-5"
+            <Stack className="w-full" margin={0} component="ul">
+              {rows.map(row => (
+                <ListItem
+                  key={row.label}
+                  className="flex flex-row w-full justify-center"
                 >
-                  <MenuItem value={dealership.city_id}>Caracas</MenuItem>
-                </Select>
-              </ListItem>
-              <ListItem>
-                <Typography variant="p" align="left" className="font-bold">
-                  Manager:
-                </Typography>
-                <Input
-                  placeholder="Inserte el ID del Manager..."
-                  value={dealership.manager_id}
-                  className="ml-5"
-                >
-                </Input>
-              </ListItem>
+                  <Typography
+                    variant="p"
+                    align="left"
+                    className="font-bold w-[15%]"
+                  >
+                    {row.label}
+                  </Typography>
+                  <Typography className="ml-5 w-[85%] flex-grow">
+                    {row.value}
+                  </Typography>
+                </ListItem>
+              ))}
             </Stack>
-          </Card>
 
-          <Box
-            component="aside"
-            className="flex flex-col gap-2 px-5 whitespace-nowrap w-[30%]"
-          >
-            <Button variant="contained" color="inherit">
-              Editar Orden
-            </Button>
-            <Button variant="contained">Agregar Pago</Button>
-          </Box>
+            <Box className="flex flex-row justify-center gap-2">
+              <Link href={pathname + '/edit'}>
+                <Button variant="contained" className="max-w-md">
+                  {toEditButtonLabel}
+                </Button>
+              </Link>
+              <Button variant="contained" color="error">
+                Eliminar
+              </Button>
+            </Box>
+          </Card>
         </Box>
       </Container>
     </Box>
