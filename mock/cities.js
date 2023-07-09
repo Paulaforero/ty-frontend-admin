61d34f0af2e3e68708dcaabf8724bc137b9ec100
Chocopaula <mariapforero2003@gmail.com>
2023-07-09 17:04:35 -0400

    feature: added mock

---
+++ mock/cities.js
@@ -0,0 +1,27 @@
+export const cities = [
+  {
+    id: 1,
+    name: 'New York City',
+    state_id: 1,
+  },
+  {
+    id: 2,
+    name: 'Los Angeles',
+    state_id: 2,
+  },
+  {
+    id: 3,
+    name: 'Chicago',
+    state_id: 3,
+  },
+  {
+    id: 4,
+    name: 'Houston',
+    state_id: 4,
+  },
+  {
+    id: 5,
+    name: 'Phoenix',
+    state_id: 5,
+  },
+]
