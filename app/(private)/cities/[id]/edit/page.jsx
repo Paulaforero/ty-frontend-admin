2aa683f7872243ced94b5ade205371a6eb081048
Chocopaula <mariapforero2003@gmail.com>
2023-07-09 18:04:42 -0400

    feature: added edit page

---
+++ app/(private)/cities/[id]/edit/page.jsx
@@ -0,0 +1,48 @@
+'use client'
+
+import EditionPageView from '@/components/edition-page-view'
+import { useState } from 'react'
+
+export default function EditPage() {
+  const city = {
+    id: 1,
+    name: 'New York City',
+    state_id: 1,
+  }
+  const [formValues, setFormValues] = useState(city)
+
+  const inputs = [
+    {
+        label: 'Nombre:',
+        type: 'text',
+        name: 'name',
+        placeholder: 'Ingrese el nombre',
+        required: true,
+      },
+      {
+        label: 'Estado:',
+        type: 'select',
+        options: states.map(state => ({
+          label: state.name,
+          value: state.id,
+        })),
+        name: 'state_id',
+        placeholder: 'Seleccione el estado',
+        required: true,
+      },
+    ]
+
+  const handleChange = event => {
+    setFormValues({ ...formValues, [event.target.name]: event.target.value })
+  }
+
+  return (
+    <EditionPageView
+      title="Editar ciudad"
+      submitLabel="Editar ciudad"
+      inputs={inputs}
+      values={formValues}
+      handleChange={handleChange}
+    />
+  )
+}
