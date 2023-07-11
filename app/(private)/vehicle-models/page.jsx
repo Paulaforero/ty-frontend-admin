22a5f63bf27ce8ad8b1cb473e564757e75c82b3e
Chocopaula <mariapforero2003@gmail.com>
2023-07-11 00:18:45 -0400

    feature: changed names in columns

--- app/(private)/vehicle-models/page.jsx
+++ app/(private)/vehicle-models/page.jsx
@@ -7,34 +7,11 @@ export default function VehicleModelsPage() {
   const columns = {
     id: 'ID',
     name: 'Nombre',
-    weight_in_kg: 'Peso (Kg)',
-    octane_rating: 'Octanaje',
-    seat_count: 'Cantidad asientos',
-    engine_oil_type: 'Tipo aceite de motor',
-  }
-
-  const filters = [
-    {
-      label: 'Filtar por octanaje',
-      options: [
-        { label: 'uno', value: 1 },
-        { label: 'dos', value: 2 },
-        { label: 'tres', value: 3 },
-        { label: 'cuatro', value: 4 },
-        { label: 'cinco', value: 5 },
-      ],
-    },
-    {
-      label: 'Filtrar por cantidad de asientos',
-      options: [
-        { label: 'uno', value: 1 },
-        { label: 'dos', value: 2 },
-        { label: 'tres', value: 3 },
-        { label: 'cuatro', value: 4 },
-        { label: 'cinco', value: 5 },
-      ],
-    },
-  ]
+    weightInKg: 'Peso (Kg)',
+    octaneRating: 'Octanaje',
+    seatCount: 'Cantidad asientos',
+    engineOilType: 'Tipo aceite de motor',
+  } 
 
   return (
     <DataPageView
