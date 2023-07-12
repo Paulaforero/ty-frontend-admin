026b51c825b39ca96644f6fee25aec3b75183826
Chocopaula <mariapforero2003@gmail.com>
2023-07-11 20:46:05 -0400

    fix: resolve conflicts

--- components/form-list-items/select-input-list-item.jsx
+++ components/form-list-items/select-input-list-item.jsx
@@ -4,11 +4,9 @@ import {
   ListItem,
   MenuItem,
   Select,
-  Typography,
 } from '@mui/material'
 
 export default function SelectInputListItem({
-  label,
   placeholder,
   value,
   options,
@@ -17,45 +15,21 @@ export default function SelectInputListItem({
   handleChange,
 }) {
   return (
-<<<<<<< Updated upstream
-    <ListItem className="flex flex-row">
-      <Typography
-        variant="p"
-        align="left"
-        className="font-bold w-[15%] text-secondary"
-      >
-        {label}
-      </Typography>
-      <FormControl fullWidth>
-        <InputLabel
-          id={`${name}-select`}
-          size="small"
-          className="items-center ml-7 lg:ml-9 w-[95%]"
-=======
     <ListItem className="flex flex-row w-full">
-      <FormControl className='w-full m-1'>
+      <FormControl className='m-1 w-full'>
         <InputLabel
           id={`${name}-select`}
           className="items-center  w-full"
->>>>>>> Stashed changes
         >
           {placeholder}
         </InputLabel>
         <Select
           id={`${name}-select`}
-<<<<<<< Updated upstream
-          size="small"
-=======
->>>>>>> Stashed changes
           value={value}
           label={placeholder}
           name={name}
           required={required}
-<<<<<<< Updated upstream
-          className="ml-7 lg:ml-9 w-[95%] text-secondary"
-=======
           className=" w-full text-secondary"
->>>>>>> Stashed changes
           onChange={handleChange}
         >
           <MenuItem value="" className="text-secondary">
