66fa3e745bc07f218a0fe96b9256625f56dd37d2
Chocopaula <mariapforero2003@gmail.com>
2023-07-11 20:33:43 -0400

    fix: resolve conflict

--- components/form-list-items/number-input-list-item.jsx
+++ components/form-list-items/number-input-list-item.jsx
@@ -1,19 +1,12 @@
-<<<<<<< Updated upstream
-import { FormControl, Input, InputAdornment, InputLabel, ListItem, Typography } from '@mui/material'
-=======
 import {
   FormControl,
-  Input,
   InputAdornment,
   InputLabel,
   ListItem,
   OutlinedInput,
-  Typography,
 } from '@mui/material'
->>>>>>> Stashed changes
 
 export default function NumberInputListItem({
-  label,
   placeholder,
   name,
   value,
@@ -22,33 +15,6 @@ export default function NumberInputListItem({
   inputProps,
   handleChange,
 }) {
-<<<<<<< Updated upstream
-     const {min, max} = inputProps
-  return (
-    <ListItem className="flex flex-row w-full justify-center">
-      <Typography
-        variant="p"
-        align="left"
-        className="font-bold w-[15%] text-secondary"
-      >
-        {label}
-      </Typography>
-      <FormControl className="ml-5 w-[85%] flex-grow" variant="standard">
-      <InputLabel htmlFor="standard-adornment-password">{placeholder}</InputLabel>
-      <Input
-        required={required}
-        className="text-secondary"
-        name={name}
-        value={value}
-        type='number'
-        inputProps={{
-            min: min,
-            max: max,
-          }}
-        onChange={handleChange}
-        startAdornment={ adornment ? <InputAdornment position="start">{adornment}</InputAdornment> : false}
-      />
-=======
   const { min, max } = inputProps
   return (
     <ListItem className="flex flex-row w-full justify-center">
@@ -74,12 +40,7 @@ export default function NumberInputListItem({
             )
           }
         />
->>>>>>> Stashed changes
       </FormControl>
     </ListItem>
   )
 }
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
