b617cfe007bc3e685bcfa5c575cc5667df1e672a
Chocopaula <mariapforero2003@gmail.com>
2023-07-11 20:34:33 -0400

    fix: resolve conflict

--- components/form-list-items/text-input-list-item.jsx
+++ components/form-list-items/text-input-list-item.jsx
@@ -1,18 +1,12 @@
-<<<<<<< Updated upstream
-import { FormControl, Input, InputAdornment, InputLabel, ListItem, Typography } from '@mui/material'
-=======
 import {
   FormControl,
   InputAdornment,
   InputLabel,
   ListItem,
   OutlinedInput,
-  Typography,
 } from '@mui/material'
->>>>>>> Stashed changes
 
 export default function TextInputListItem({
-  label,
   placeholder,
   name,
   value,
@@ -22,25 +16,6 @@ export default function TextInputListItem({
 }) {
   return (
     <ListItem className="flex flex-row w-full justify-center">
-<<<<<<< Updated upstream
-      <Typography
-        variant="p"
-        align="left"
-        className="font-bold w-[15%] text-secondary"
-      >
-        {label}
-      </Typography>
-      <FormControl className="ml-5 w-[85%] flex-grow" variant="standard">
-      <InputLabel htmlFor="standard-adornment-text">{placeholder}</InputLabel>
-      <Input
-        required={ required ? required : false}
-        className="text-secondary"
-        name={name}
-        value={value}
-        onChange={handleChange}
-        startAdornment={ adornment ? <InputAdornment position="start">{adornment}</InputAdornment> : false}
-      />
-=======
       <FormControl className='m-1 w-full'>
         <InputLabel htmlFor="outlined-adornment-text">{placeholder}</InputLabel>
           <OutlinedInput
@@ -58,7 +33,6 @@ export default function TextInputListItem({
             }
             label={placeholder}
           />
->>>>>>> Stashed changes
       </FormControl>
     </ListItem>
   )
