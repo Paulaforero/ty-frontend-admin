c0ea7644316a041b0caedd35997241c7e121257b
Chocopaula <mariapforero2003@gmail.com>
2023-07-11 20:25:54 -0400

    feature: changed input and form control in select input list item

--- components/form-list-items/select-input-list-item.jsx
+++ components/form-list-items/select-input-list-item.jsx
@@ -17,6 +17,7 @@ export default function SelectInputListItem({
   handleChange,
 }) {
   return (
+<<<<<<< Updated upstream
     <ListItem className="flex flex-row">
       <Typography
         variant="p"
@@ -30,17 +31,31 @@ export default function SelectInputListItem({
           id={`${name}-select`}
           size="small"
           className="items-center ml-7 lg:ml-9 w-[95%]"
+=======
+    <ListItem className="flex flex-row w-full">
+      <FormControl className='w-full m-1'>
+        <InputLabel
+          id={`${name}-select`}
+          className="items-center  w-full"
+>>>>>>> Stashed changes
         >
           {placeholder}
         </InputLabel>
         <Select
           id={`${name}-select`}
+<<<<<<< Updated upstream
           size="small"
+=======
+>>>>>>> Stashed changes
           value={value}
           label={placeholder}
           name={name}
           required={required}
+<<<<<<< Updated upstream
           className="ml-7 lg:ml-9 w-[95%] text-secondary"
+=======
+          className=" w-full text-secondary"
+>>>>>>> Stashed changes
           onChange={handleChange}
         >
           <MenuItem value="" className="text-secondary">
