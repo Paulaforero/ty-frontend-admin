d04e78fba87c05eff21cc04e3e5fefb3909e28cf
Chocopaula <mariapforero2003@gmail.com>
2023-07-13 23:45:23 -0400

    feature: added hooks creation

---
+++ components/vehicle-models/hooks/use-vehicle-model-creation-page.jsx
@@ -0,0 +1,81 @@
+'use client'
+
+import { useState } from 'react'
+import useSnackbar from '@/hooks/use-snackbar'
+
+export default function useVehicleModelCreationPage() {
+  
+
+  const [formValues, setFormValues] = useState({
+    name: '',
+    seatCount: '',
+    weightInKg: '',
+    octaneRating: '',
+    gearboxOilType: '',
+    engineOilType: '',
+    engineCoolantType: '',
+  })
+
+  const inputs = [
+    {
+        label: 'Nombre',
+        type: 'text',
+        name: 'name',
+        required: true,
+      },
+      {
+          type: 'number',
+          name: 'seatCount',
+          label: 'Cantidad de asientos',
+          min: 2,
+          max: 8,
+          required: true,
+        },
+      {
+          type: 'select',
+          options: [
+          {
+            label: '95',
+            value: 95,
+          },
+          {
+            label: '91',
+            value: 91,
+          },
+        ],
+        name: 'octaneRating',
+        label: 'Octanaje',
+        required: true,
+      },
+      {
+          type: 'text',
+          name: 'gearboxOilType',
+          label: 'Tipo de aceite de caja',
+          required: true,
+        },
+        {
+          type: 'text',
+          name: 'engineOilType',
+          label: 'Tipo de aceite de motor',
+          required: true,
+        },
+        {
+          type: 'text',
+          name: 'engineCoolantType',
+          label: 'Tipo de refrigerante',
+          required: true,
+        },
+  ]
+
+  const handleChange = event => {
+    setFormValues({ ...formValues, [event.target.name]: event.target.value })
+  }
+
+
+  return {
+    inputs,
+    formValues,
+    handleChange,
+  }
+}
+
