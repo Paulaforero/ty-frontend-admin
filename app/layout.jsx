0570896229a74d9afb22037b0e937f317c8b24a7
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-07-15 05:14:26 -0400

    fix: fixed bad merge in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,8 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <ThemeProvider theme={theme}>
-        <body className={inter.className + ' h-screen w-screen'}>
-          <SnackbarProvide><motion.div className='h-screen w-screen p-0 m-0' animate={{opacity: 1, animationDelay: 4}} initial={{opacity: 0}}>{children}</motion.div></SnackbarProvide>
+        <body className={'h-screen w-screen'}>
+          <SnackbarProvider><motion.div className='h-screen w-screen p-0 m-0' animate={{opacity: 1, animationDelay: 4}} initial={{opacity: 0}}>{children}</motion.div></SnackbarProvider>
         </body>
       </ThemeProvider>
     </html>
