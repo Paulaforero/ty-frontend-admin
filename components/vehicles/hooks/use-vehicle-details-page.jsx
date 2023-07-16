82ae84bdadeba6f0b9b624780941391cc6adae35
Chocopaula <mariapforero2003@gmail.com>
2023-07-16 15:43:06 -0400

    feature: added midleware in details page

--- components/vehicles/hooks/use-vehicle-details-page.jsx
+++ components/vehicles/hooks/use-vehicle-details-page.jsx
@@ -2,13 +2,17 @@
 
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { BACKEND_URLS } from '@/utils/backend-urls'
 import useSnackbar from '@/hooks/use-snackbar'
 
 export default function useVehicleDetailsPage() {
+  const router = useRouter()
+  const notify = useSnackbar()
 
   const searchParams = useSearchParams()
 
-  const id = searchParams.get('id')
+  const plate = searchParams.get('plate')
+
 
   const [vehicleData, setVehicleData] = useState({
     plate: '',
@@ -65,11 +69,67 @@ export default function useVehicleDetailsPage() {
     ],
     [vehicleData]
   )
-
-  const handleChange = event => {
-    setVehicleModel({ ...vehicleModelData, [event.target.name]: event.target.value })
+  const handleSubmit = e => {
+    e.preventDefault()
+    editVehicle()
   }
 
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`${BACKEND_URLS.vehicles}?plate=${plate}`, {
+        method: 'DELETE',
+        cache: 'no-store',
+      })
 
-  return { rows, vehicleData, handleChange, isLoading, id }
+      if (!response.ok) throw new Error()
+
+      notify({
+        message: '¡El vehículo se ha eliminado exitósamente!',
+        severity: 'success',
+      })
+
+      router.push('/cities')
+    } catch (error) {
+      notify({
+        message: 'Error al intentar eliminar el vehículo.',
+        severity: 'error',
+      })
+    }
+  }
+
+  const fetchVehicleData = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await fetch(
+        `${BACKEND_URLS.vehicles}/view?plate=${plate}`,
+        {
+          method: 'GET',
+          cache: 'no-store',
+        }
+      )
+
+      const responseData = await response.json()
+      const fetchedVehicleData = responseData.data
+      setVehicleData(fetchedVehicleData)
+    } catch (error) {
+      notify({
+        message: 'Error obteniendo los datos del vehículo.',
+        severity: 'error',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }, [notify, plate])
+
+  useEffect(() => {
+    fetchVehicleData()
+  }, [fetchVehicleData])
+
+  return {
+    rows,
+    vehicleData,
+    handleDelete,
+    isLoading,
+    plate: vehicleData.plate,
+  }
 }
