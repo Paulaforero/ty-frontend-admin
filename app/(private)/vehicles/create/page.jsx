14d7fb8a75a75f7c40de07cc194c7e6cfdc979aa
Chocopaula <mariapforero2003@gmail.com>
2023-07-16 15:40:05 -0400

    feature: changed view page

--- app/(private)/vehicles/create/page.jsx
+++ app/(private)/vehicles/create/page.jsx
@@ -4,10 +4,7 @@ import CreationPageView from '@/components/creation-page-view'
 import useVehicleCreationPage from '@/components/vehicles/hooks/use-vehicle-creation-page'
 
 export default function CreatePage() {
-  const { inputs,
-    formValues,
-    handleChange,
-    } =
+  const { inputs, formValues, handleChange, handleSubmit } =
     useVehicleCreationPage()
 
   return (
@@ -17,6 +14,7 @@ export default function CreatePage() {
       inputs={inputs}
       values={formValues}
       handleChange={handleChange}
+      handleSubmit={handleSubmit}
     />
   )
 }
