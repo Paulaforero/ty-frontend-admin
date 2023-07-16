0d3b917a2588ff9d59bd4a18f25f8e61368f1b53
Chocopaula <mariapforero2003@gmail.com>
2023-07-16 14:59:33 -0400

    feature: middleware in hooks edition  page

--- components/vehicle-models/hooks/use-vehicle-model-edition-page.jsx
+++ components/vehicle-models/hooks/use-vehicle-model-edition-page.jsx
@@ -2,14 +2,17 @@
 
 import { useCallback, useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
+import { BACKEND_URLS } from '@/utils/backend-urls'
 import { useRouter } from 'next/navigation'
 import useSnackbar from '@/hooks/use-snackbar'
 
 export default function useVehicleModelEditionPage() {
+  const router = useRouter()
+  const notify = useSnackbar()
 
   const searchParams = useSearchParams()
-
   const id = searchParams.get('id')
+  
 
   const [formValues, setFormValues] = useState({
     id,
@@ -73,11 +76,81 @@ export default function useVehicleModelEditionPage() {
         required: true,
       },
   ]
-
   const handleChange = event => {
     setFormValues({ ...formValues, [event.target.name]: event.target.value })
   }
 
+  const fetchVehicleModelData = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await fetch(
+        `${BACKEND_URLS.cities}/view?id=${id}`,
+        {
+          method: 'GET',
+          cache: 'no-store',
+        }
+      )
 
-  return { inputs, formValues, handleChange, isLoading, id }
+      const responseData = await response.json()
+      const fetchedVehicleModelData = responseData.data
+
+      setFormValues(fetchedVehicleModelData)
+    } catch (error) {
+      notify({
+        message: 'Error obteniendo los datos del modelo del vehículo.',
+        severity: 'error',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }, [notify, id])
+
+  useEffect(() => {
+    fetchVehicleModelData()
+  }, [fetchVehicleModelData])
+
+  const editVehicleModel = async () => {
+    try {
+      const { id, ...toEditValues } = formValues
+      const response = await fetch(
+        `${BACKEND_URLS.vehicleModels}?id=${id}`,
+        {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(toEditValues),
+          cache: 'no-store',
+        }
+      )
+
+      const responseData = await response.json()
+      console.log(responseData)
+
+      if (!response.ok) throw new Error()
+
+      notify({
+        message: '¡Se ha editado el modelo de vehículo exitosamente!',
+        severity: 'success',
+      })
+
+      router.push('/vehicle-models')
+    } catch (error) {
+      notify({
+        message: 'Error al intentar editar el modelo de vehículo.',
+        severity: 'error',
+      })
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    editVehicleModel()
+  }
+
+  useEffect(() => {
+    fetchVehicleModelData()
+  }, [fetchVehicleModelData])
+
+  return { inputs, formValues, handleChange, handleSubmit, isLoading }
 }
